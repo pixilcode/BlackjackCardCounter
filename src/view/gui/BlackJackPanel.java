f2181238bee3723bc69c104959bf4ad72943e311
zeajenson <zeajenson@gmail.com>
2016-12-12 08:35:34 -0700

    changed some buttons

--- src/view/gui/BlackJackPanel.java
+++ src/view/gui/BlackJackPanel.java
@@ -53,9 +53,6 @@ public class BlackJackPanel extends GamePanel{
 		this.initLabels();
 		
 		this.baseLayout = new SpringLayout();
-
-
-		
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -112,6 +109,20 @@ public class BlackJackPanel extends GamePanel{
 		this.add(winningPercentLabel);
 		this.add(lessThanPercentLabel);
 		this.add(greaterThanPercentLabel);
+		this.add(addCardButtons[0]);
+		this.add(addCardButtons[1]);
+		this.add(addCardButtons[2]);
+		this.add(addCardButtons[3]);
+		this.add(addCardButtons[4]);
+		this.add(addCardButtons[5]);
+		this.add(addCardButtons[6]);
+		this.add(addCardButtons[7]);
+		this.add(addCardButtons[8]);
+		this.add(addCardButtons[9]);
+		this.add(addCardButtons[10]);
+		this.add(addCardButtons[11]);
+		this.add(addCardButtons[12]);
+		
 	}
 	
 	private void setupLayout(){
@@ -143,46 +154,45 @@ public class BlackJackPanel extends GamePanel{
 		addThree.setText("Three");
 		addTwo.setText("Two");
 		addAce.setText("Ace");
-		
+		baseLayout.putConstraint(SpringLayout.WEST, addFour, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, addFour, -50, SpringLayout.WEST, addKing);
+		baseLayout.putConstraint(SpringLayout.NORTH, addKing, 6, SpringLayout.SOUTH, addQueen);
+		baseLayout.putConstraint(SpringLayout.EAST, addKing, -250, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addQueen, 6, SpringLayout.SOUTH, addJack);
+		baseLayout.putConstraint(SpringLayout.EAST, addQueen, -250, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, addTen, -6, SpringLayout.NORTH, addJack);
 		baseLayout.putConstraint(SpringLayout.WEST, addTwo, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, addJack, 164, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, addFour, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, addKing, 6, SpringLayout.SOUTH, addQueen);
-		baseLayout.putConstraint(SpringLayout.EAST, addKing, 164, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, addKing, 99, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, addJack, 99, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, addTwo, -50, SpringLayout.WEST, addJack);
+		baseLayout.putConstraint(SpringLayout.EAST, addJack, -250, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, addKing, 130, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.WEST, addJack, 130, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, addTen, -250, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addNine, 6, SpringLayout.SOUTH, addEight);
+		baseLayout.putConstraint(SpringLayout.EAST, addNine, 80, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addEight, 6, SpringLayout.SOUTH, addSeven);
+		baseLayout.putConstraint(SpringLayout.EAST, addEight, -370, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addSeven, 6, SpringLayout.SOUTH, addSix);
+		baseLayout.putConstraint(SpringLayout.EAST, addSeven, 80, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addSix, 6, SpringLayout.SOUTH, addFive);
+		baseLayout.putConstraint(SpringLayout.EAST, addSix, -370, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, addFive, -370, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addFive, 6, SpringLayout.SOUTH, addFour);
+		baseLayout.putConstraint(SpringLayout.WEST, addQueen, 50, SpringLayout.EAST, addThree);
+		baseLayout.putConstraint(SpringLayout.NORTH, addFour, 6, SpringLayout.SOUTH, addThree);
+		baseLayout.putConstraint(SpringLayout.EAST, addThree, 80, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, addJack, 58, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addThree, 6, SpringLayout.SOUTH, addTwo);
+		baseLayout.putConstraint(SpringLayout.SOUTH, addAce, -6, SpringLayout.NORTH, addTwo);
+		baseLayout.putConstraint(SpringLayout.WEST, addTen, 50, SpringLayout.EAST, addAce);
+		baseLayout.putConstraint(SpringLayout.EAST, addAce, 80, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addNine, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addSeven, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addThree, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, addTwo, 58, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addAce, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, addNine, 6, SpringLayout.SOUTH, addEight);
-		baseLayout.putConstraint(SpringLayout.EAST, addNine, 75, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, addEight, -264, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.WEST, addTen, 24, SpringLayout.EAST, addAce);
-		baseLayout.putConstraint(SpringLayout.EAST, addTen, -175, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addEight, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addSix, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, addSix, -264, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, addFive, 10, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, addFive, -264, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.EAST, addAce, 75, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, addThree, 6, SpringLayout.SOUTH, addTwo);
-		baseLayout.putConstraint(SpringLayout.SOUTH, addAce, -6, SpringLayout.NORTH, addTwo);
-		baseLayout.putConstraint(SpringLayout.EAST, addTwo, -24, SpringLayout.WEST, addJack);
-		baseLayout.putConstraint(SpringLayout.NORTH, addQueen, 6, SpringLayout.SOUTH, addJack);
-		baseLayout.putConstraint(SpringLayout.WEST, addQueen, 24, SpringLayout.EAST, addThree);
-		baseLayout.putConstraint(SpringLayout.EAST, addQueen, -175, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, addFour, 6, SpringLayout.SOUTH, addThree);
-		baseLayout.putConstraint(SpringLayout.EAST, addThree, 75, SpringLayout.WEST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, addFive, 6, SpringLayout.SOUTH, addFour);
-		baseLayout.putConstraint(SpringLayout.EAST, addFour, -24, SpringLayout.WEST, addKing);
-		baseLayout.putConstraint(SpringLayout.NORTH, addSix, 6, SpringLayout.SOUTH, addFive);
-		baseLayout.putConstraint(SpringLayout.NORTH, addSeven, 6, SpringLayout.SOUTH, addSix);
-		baseLayout.putConstraint(SpringLayout.NORTH, addEight, 6, SpringLayout.SOUTH, addSeven);
-		baseLayout.putConstraint(SpringLayout.EAST, addSeven, 75, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, amountOfAces, 4, SpringLayout.NORTH, addAce);
 		baseLayout.putConstraint(SpringLayout.WEST, amountOfAces, 6, SpringLayout.EAST, addAce);
 		baseLayout.putConstraint(SpringLayout.NORTH, amountOfTwos, 4, SpringLayout.NORTH, addTwo);
