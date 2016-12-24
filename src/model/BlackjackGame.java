e8356d7942b92b51966ba7b17d2ce1e3fc41ae0b
zeajenson <zeajenson@gmail.com>
2016-12-24 16:22:43 -0700

    finished deck and started changing the black jack game class

--- src/model/BlackjackGame.java
+++ src/model/BlackjackGame.java
@@ -1,219 +1,247 @@
 package model;
 
-import model.cardDataTypes.Card;
-import model.cardDataTypes.Deck;
-import view.User;
+import model.*;
+import model.cardDataTypes.*;
+import view.*;
 
-public class BlackjackGame extends Deck {
-	
+public class BlackjackGame{
 	static final int ACE = 1;
 	static final int JACK = 10;
 	static final int QUEEN = 10;
 	static final int KING = 10;
-	private static int possibleSums = 0;
-	private Card[] cards;
 	
-	public BlackjackGame() {
-		
-		super(false);
-		
-		cards = (Card[]) unseenDeck.toArray();
-		
-		for(int i = 0; i < cards.length; i++) {
-			
-			if(cards[i].getSymbol().equals("J") || cards[i].getSymbol().equals("Q") || cards[i].getSymbol().equals("K")) {
-				
-				unseenDeck.remove(cards[i]);
-				unseenDeck.add(new Card(Suit.valueOf(cards[i].getSuit()), cards[i].getSymbol(), 10));
-				
-			}
-			
-		}
-		
-		unseenDeck.sort(null);
-		
+	CardDeck deck;
+	
+	public BlackjackGame(){
+		deck = new CardDeck();
 	}
 	
-	//Add BlackjackProbCalculator methods here
-	//Also, add a 'getNextCard' method to unseenDeck
 	private static float toPercent(float num) {
-		
-		float percent = 0;
-		
-		try {
-			
-			percent = ((float) ((int) (num * 10000))) / 100;
-			
-		} catch(NumberFormatException nfe) {
-			
-		}
-		
-		return percent;
+	float percent = 0;	
+	try {
+		percent = ((float) ((int) (num * 10000))) / 100;
+	} catch(NumberFormatException nfe) {
 		
 	}
 	
-	public static Suit getSuit(String suit) {
-		
-		switch(suit.toUpperCase()) {
-		
-		case "SPADES":
-			return Suit.SPADES;
-		
-		case "CLUBS":
-			return Suit.CLUBS;
-		
-		case "DIAMONDS":
-			return Suit.DIAMONDS;
-		
-		case "HEARTS":
-			return Suit.HEARTS;
-		
-		default:
-			return null;
-		
-		}
-		
-	}
 	
-	private int sumsLessThan21(int knownSum) {
-		
-		int lessThan = 21 - knownSum;
-		Card[] sumOf21;
-		sumOf21 = toArray();
-		int lessThan21 = 0;
-		
-		for(int i = 0; i < sumOf21.length; i++) {
-			
-			for(int j = 0; j < sumOf21.length; j++) {
-				
-				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() < lessThan) {
-					
-					lessThan21++;
-					
-				}
-			
-			}
-			
-		}
-		
-		sums(knownSum);
-		
-		return lessThan21;
-		
-	}
 	
-	private int sumsEqualTo21(int knownSum) {
-		
-		int equalTo = 21 - knownSum;
-		Card[] sumOf21;
-		sumOf21 = toArray();
-		int equalTo21 = 0;
-		
-		for(int i = 0; i < sumOf21.length; i++) {
-			
-			for(int j = 0; j < sumOf21.length; j++) {
-				
-				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() == equalTo) {
-					
-					equalTo21++;
-					
-				}
-			
-			}
-			
-		}
-		
-		sums(knownSum);
-		
-		return equalTo21;
-		
-	}
-	
-	private int sumsGreaterThan21(int knownSum) {
-		
-		int greaterThan = 21 - knownSum;
-		Card[] sumOf21;
-		sumOf21 = toArray();
-		int greaterThan21 = 0;
-		
-		for(int i = 0; i < sumOf21.length; i++) {
-			
-			for(int j = 0; j < sumOf21.length; j++) {
-				
-				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() < greaterThan) {
-					
-					greaterThan21++;
-					
-				}
-			
-			}
-			
-		}
-		
-		sums(knownSum);
-		
-		return greaterThan21;
-		
-	}
-	
-	public float possibilityLessThan21(int knownSum) {
-		
-		return toPercent((float) (sumsLessThan21(knownSum)) / possibleSums);
-		
-	}
-	
-	public float possibilityEqualTo21(int knownSum) {
-		
-		return toPercent((float) (sumsEqualTo21(knownSum)) / possibleSums);
-		
-	}
-	
-	public float possibilityGreaterThan21(int knownSum) {
-		
-		return toPercent((float) (sumsGreaterThan21(knownSum)) / possibleSums);
-		
-	}
-	
-	private void sums(int knownSum) {
-		
-		Card[] sumOf21;
-		sumOf21 = toArray();
-		possibleSums = 0;
-		
-		for(int i = 0; i < sumOf21.length; i++) {
-			
-			for(int j = 0; j < sumOf21.length; j++) {
-				
-				if(i != j) {
-					
-					possibleSums++;
-					
-				}
-			
-			}
-			
-		}
-		
-	}
-	
-	public float cardProbability(String symbol) {
-		
-		Card[] unseenDeckArray = toArray();
-		float numberOfCards = 0;
-		
-		for(int i = 0; i < unseenDeckArray.length; i++) {
-			
-			if(unseenDeckArray[i].getSymbol() == symbol) {
-				
-				numberOfCards++;
-				
-				
-			}
-			
-		}
-		
-		return toPercent(numberOfCards / length());
-		
-	}
+	return percent;
 	
 }
+}
+
+
+//public class BlackjackGame extends Deck {
+//	
+//	static final int ACE = 1;
+//	static final int JACK = 10;
+//	static final int QUEEN = 10;
+//	static final int KING = 10;
+//	private static int possibleSums = 0;
+//	private Card[] cards;
+//	
+//	public BlackjackGame() {
+//		
+//		super(false);
+//		
+//		cards = (Card[]) unseenDeck.toArray();
+//		
+//		for(int i = 0; i < cards.length; i++) {
+//			
+//			if(cards[i].getSymbol().equals("J") || cards[i].getSymbol().equals("Q") || cards[i].getSymbol().equals("K")) {
+//				
+//				unseenDeck.remove(cards[i]);
+//				unseenDeck.add(new Card(Suit.valueOf(cards[i].getSuit()), cards[i].getSymbol(), 10));
+//				
+//			}
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	//Add BlackjackProbCalculator methods here
+//	//Also, add a 'getNextCard' method to unseenDeck
+//	private static float toPercent(float num) {
+//		
+//		float percent = 0;
+//		
+//		try {
+//			
+//			percent = ((float) ((int) (num * 10000))) / 100;
+//			
+//		} catch(NumberFormatException nfe) {
+//			
+//		}
+//		
+//		return percent;
+//		
+//	}
+//	
+//	public static Suit getSuit(String suit) {
+//		
+//		switch(suit.toUpperCase()) {
+//		
+//		case "SPADES":
+//			return Suit.SPADES;
+//		
+//		case "CLUBS":
+//			return Suit.CLUBS;
+//		
+//		case "DIAMONDS":
+//			return Suit.DIAMONDS;
+//		
+//		case "HEARTS":
+//			return Suit.HEARTS;
+//		
+//		default:
+//			return null;
+//		
+//		}
+//		
+//	}
+//	
+//	private int sumsLessThan21(int knownSum) {
+//		
+//		int lessThan = 21 - knownSum;
+//		Card[] sumOf21;
+//		sumOf21 = toArray();
+//		int lessThan21 = 0;
+//		
+//		for(int i = 0; i < sumOf21.length; i++) {
+//			
+//			for(int j = 0; j < sumOf21.length; j++) {
+//				
+//				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() < lessThan) {
+//					
+//					lessThan21++;
+//					
+//				}
+//			
+//			}
+//			
+//		}
+//		
+//		sums(knownSum);
+//		
+//		return lessThan21;
+//		
+//	}
+//	
+//	private int sumsEqualTo21(int knownSum) {
+//		
+//		int equalTo = 21 - knownSum;
+//		Card[] sumOf21;
+//		sumOf21 = toArray();
+//		int equalTo21 = 0;
+//		
+//		for(int i = 0; i < sumOf21.length; i++) {
+//			
+//			for(int j = 0; j < sumOf21.length; j++) {
+//				
+//				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() == equalTo) {
+//					
+//					equalTo21++;
+//					
+//				}
+//			
+//			}
+//			
+//		}
+//		
+//		sums(knownSum);
+//		
+//		return equalTo21;
+//		
+//	}
+//	
+//	private int sumsGreaterThan21(int knownSum) {
+//		
+//		int greaterThan = 21 - knownSum;
+//		Card[] sumOf21;
+//		sumOf21 = toArray();
+//		int greaterThan21 = 0;
+//		
+//		for(int i = 0; i < sumOf21.length; i++) {
+//			
+//			for(int j = 0; j < sumOf21.length; j++) {
+//				
+//				if(i != j && sumOf21[i].getValue() + sumOf21[j].getValue() < greaterThan) {
+//					
+//					greaterThan21++;
+//					
+//				}
+//			
+//			}
+//			
+//		}
+//		
+//		sums(knownSum);
+//		
+//		return greaterThan21;
+//		
+//	}
+//	
+//	public float possibilityLessThan21(int knownSum) {
+//		
+//		return toPercent((float) (sumsLessThan21(knownSum)) / possibleSums);
+//		
+//	}
+//	
+//	public float possibilityEqualTo21(int knownSum) {
+//		
+//		return toPercent((float) (sumsEqualTo21(knownSum)) / possibleSums);
+//		
+//	}
+//	
+//	public float possibilityGreaterThan21(int knownSum) {
+//		
+//		return toPercent((float) (sumsGreaterThan21(knownSum)) / possibleSums);
+//		
+//	}
+//	
+//	private void sums(int knownSum) {
+//		
+//		Card[] sumOf21;
+//		sumOf21 = toArray();
+//		possibleSums = 0;
+//		
+//		for(int i = 0; i < sumOf21.length; i++) {
+//			
+//			for(int j = 0; j < sumOf21.length; j++) {
+//				
+//				if(i != j) {
+//					
+//					possibleSums++;
+//					
+//				}
+//			
+//			}
+//			
+//		}
+//		
+//	}
+//	
+//	public float cardProbability(String symbol) {
+//		
+//		Card[] unseenDeckArray = toArray();
+//		float numberOfCards = 0;
+//		
+//		for(int i = 0; i < unseenDeckArray.length; i++) {
+//			
+//			if(unseenDeckArray[i].getSymbol() == symbol) {
+//				
+//				numberOfCards++;
+//				
+//				
+//			}
+//			
+//		}
+//		
+//		return toPercent(numberOfCards / length());
+//		
+//	}
+//	
+//}
--- src/model/cardDataTypes/Deck.java
+++ src/model/cardDataTypes/Deck.java
@@ -4,247 +4,247 @@ import java.util.ArrayList;
 
 import model.Suit;
 
-public class Deck {
-	
-	//Declare variables
-	protected ArrayList<Card> unseenDeck;
-	protected ArrayList<Card> seenDeck;
-	
-	//Create an array with standard strings in it
-	private static Cards cardSymbols;
-	protected static final String[] standardCardSymbols = {"A","1","2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING"};
-	
-	//Create variables that store King, Queen, Jack, and Ace Values
-	final int ACE = 1;
-	final int JACK = 11;
-	final int QUEEN = 12;
-	final int KING = 13;
-	final int JOKER = 14;
-	
-	//A constructor that gives the deck the four suits
-	public Deck(boolean addJokers) {
-		
-		
-		unseenDeck = new ArrayList<Card>();
-		seenDeck = new ArrayList<Card>();
-		for(int i = 0; i < 52; i++) {
-			
-			Suit suit = Suit.SPADES;
-			
-			//Set the suit according to 'i'
-			switch((i % 13)) {
-			
-			case 0:
-				suit = Suit.SPADES;
-				break;
-			
-			case 1:
-				suit = Suit.CLUBS;
-				break;
-			
-			case 2:
-				suit = Suit.DIAMONDS;
-				break;
-			
-			case 3:
-				suit = Suit.HEARTS;
-				break;
-			
-			}
-			
-			unseenDeck.add(new Card(suit, standardCardSymbols[i], i + 1));
-			
-		}
-		
-		//If they want Jokers, add Jokers
-		if(addJokers) {
-			
-			unseenDeck.add(new Card(Suit.JOKER, "JK", 0));
-			unseenDeck.add(new Card(Suit.JOKER, "JK", 0));
-			
-		}
-		
-		unseenDeck.sort(null);
-		
-	}
-	
-	public int length() {
-		
-		return unseenDeck.size();
-		
-	}
-
-	public String toString() {
-		
-		return unseenDeck.toString();
-		
-	}
-	
-	public Card[] toArray() {
-		
-		Card[] deckArray = new Card[unseenDeck.size()];
-		unseenDeck.toArray(deckArray);
-		
-		return deckArray;
-		
-	}
-	
-	public int getSymbolValue(String symbol) {
-		
-		if(symbol.equals("A")) {
-			
-			return this.ACE;
-		
-		} else if(symbol.equals("J")) {
-			
-			return this.JACK;
-			
-		} else if(symbol.equals("Q")) {
-			
-			return this.QUEEN;
-			
-		} else if(symbol.equals("K")) {
-			
-			return this.KING;
-			
-		} else if(symbol.equals("JK")) {
-			
-			return this.JOKER;
-			
-		}
-		
-		return Integer.parseInt(symbol);
-		
-	}
-	
-	public void addToDeck(Card card) {
-		
-		unseenDeck.add(card);
-		unseenDeck.sort(null);
-		
-	}
-	
-	public void addToDeck(String suit, String symbol, int value) {
-		
-		addToDeck(new Card(Suit.valueOf(suit), symbol, value));
-		
-	}
-	
-	public void addDeck(Deck deck) {
-		
-		Card[] deckArray = deck.toArray();
-		
-		for(int i = 0; i < deckArray.length; i++) {
-			
-			unseenDeck.add(deckArray[i]);
-			
-		}
-		
-		unseenDeck.sort(null);
-		
-	}
-	
-	public void removeFromDeck(Card card) {
-		
-		if(unseenDeck.contains(card)) {
-			
-			unseenDeck.remove(card);
-			
-		} else if(seenDeck.contains(card)) {
-			
-			seenDeck.remove(card);
-			
-		}
-		
-		unseenDeck.sort(null);
-		
-	}
-	
-	public void removeFromDeck(String suit, String symbol, int value) {
-		
-		removeFromDeck(new Card(Suit.valueOf(suit), symbol, value));
-		
-	}
-	
-	public void add(Card card) {
-		
-		if(seenDeck.contains(card)) {
-			
-			seenDeck.remove(card);
-			unseenDeck.add(card);
-			
-		}
-		
-		unseenDeck.sort(null);
-		
-	}
-	
-	public void add(String suit, String symbol, int value) {
-		
-		add(new Card(Suit.valueOf(suit), symbol, value));
-		
-	}
-	
-	public void reset() {
-		
-		for(int i = 0; i < seenDeck.size(); i++) {
-			
-			unseenDeck.add(seenDeck.get(i));
-			
-		}
-		
-		unseenDeck.sort(null);
-		seenDeck.clear();
-		
-	}
-	
-	public void remove(Card card) {
-		
-		if(unseenDeck.contains(card)) {
-			
-			unseenDeck.remove(card);
-			seenDeck.add(card);
-			
-		}
-		
-		unseenDeck.sort(null);
-		
-	}
-	
-	public void remove(String suit, String symbol, int value) {
-		
-		remove(new Card(Suit.valueOf(suit), symbol, value));
-		
-	}
-	
-	public void remove(String symbol) {
-		
-		if(unseenDeck.contains(new Card(Suit.SPADES, symbol, getSymbolValue(symbol)))) {
-			
-			remove(Suit.SPADES.toString(), symbol, getSymbolValue(symbol));
-			
-		} else if(unseenDeck.contains(new Card(Suit.CLUBS, symbol, getSymbolValue(symbol)))) {
-			
-			remove(Suit.CLUBS.toString(), symbol, getSymbolValue(symbol));
-			
-		} else if(unseenDeck.contains(new Card(Suit.DIAMONDS, symbol, getSymbolValue(symbol)))) {
-			
-			remove(Suit.DIAMONDS.toString(), symbol, getSymbolValue(symbol));
-			
-		} else if(unseenDeck.contains(new Card(Suit.HEARTS, symbol, getSymbolValue(symbol)))) {
-			
-			remove(Suit.HEARTS.toString(), symbol, getSymbolValue(symbol));
-			
-		}
-		
-	}
-	
-	public void undo() {
-		
-		Card undoneCard = seenDeck.remove(seenDeck.size() - 1);
-		unseenDeck.add(undoneCard);
-		unseenDeck.sort(null);
-		
-	}
-	
-}
+//public class Deck {
+//	
+//	//Declare variables
+//	protected ArrayList<Card> unseenDeck;
+//	protected ArrayList<Card> seenDeck;
+//	
+//	//Create an array with standard strings in it
+//	private static Cards cardSymbols;
+//	protected static final String[] standardCardSymbols = {"A","1","2","3","4","5","6","7","8","9","10","JACK","QUEEN","KING"};
+//	
+//	//Create variables that store King, Queen, Jack, and Ace Values
+//	final int ACE = 1;
+//	final int JACK = 11;
+//	final int QUEEN = 12;
+//	final int KING = 13;
+//	final int JOKER = 14;
+//	
+//	//A constructor that gives the deck the four suits
+//	public Deck(boolean addJokers) {
+//		
+//		
+//		unseenDeck = new ArrayList<Card>();
+//		seenDeck = new ArrayList<Card>();
+//		for(int i = 0; i < 52; i++) {
+//			
+//			Suit suit = Suit.SPADES;
+//			
+//			//Set the suit according to 'i'
+//			switch((i % 13)) {
+//			
+//			case 0:
+//				suit = Suit.SPADES;
+//				break;
+//			
+//			case 1:
+//				suit = Suit.CLUBS;
+//				break;
+//			
+//			case 2:
+//				suit = Suit.DIAMONDS;
+//				break;
+//			
+//			case 3:
+//				suit = Suit.HEARTS;
+//				break;
+//			
+//			}
+//			
+//			unseenDeck.add(new Card(suit, standardCardSymbols[i], i + 1));
+//			
+//		}
+//		
+//		//If they want Jokers, add Jokers
+//		if(addJokers) {
+//			
+//			unseenDeck.add(new Card(Suit.JOKER, "JK", 0));
+//			unseenDeck.add(new Card(Suit.JOKER, "JK", 0));
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public int length() {
+//		
+//		return unseenDeck.size();
+//		
+//	}
+//
+//	public String toString() {
+//		
+//		return unseenDeck.toString();
+//		
+//	}
+//	
+//	public Card[] toArray() {
+//		
+//		Card[] deckArray = new Card[unseenDeck.size()];
+//		unseenDeck.toArray(deckArray);
+//		
+//		return deckArray;
+//		
+//	}
+//	
+//	public int getSymbolValue(String symbol) {
+//		
+//		if(symbol.equals("A")) {
+//			
+//			return this.ACE;
+//		
+//		} else if(symbol.equals("J")) {
+//			
+//			return this.JACK;
+//			
+//		} else if(symbol.equals("Q")) {
+//			
+//			return this.QUEEN;
+//			
+//		} else if(symbol.equals("K")) {
+//			
+//			return this.KING;
+//			
+//		} else if(symbol.equals("JK")) {
+//			
+//			return this.JOKER;
+//			
+//		}
+//		
+//		return Integer.parseInt(symbol);
+//		
+//	}
+//	
+//	public void addToDeck(Card card) {
+//		
+//		unseenDeck.add(card);
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public void addToDeck(String suit, String symbol, int value) {
+//		
+//		addToDeck(new Card(Suit.valueOf(suit), symbol, value));
+//		
+//	}
+//	
+//	public void addDeck(Deck deck) {
+//		
+//		Card[] deckArray = deck.toArray();
+//		
+//		for(int i = 0; i < deckArray.length; i++) {
+//			
+//			unseenDeck.add(deckArray[i]);
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public void removeFromDeck(Card card) {
+//		
+//		if(unseenDeck.contains(card)) {
+//			
+//			unseenDeck.remove(card);
+//			
+//		} else if(seenDeck.contains(card)) {
+//			
+//			seenDeck.remove(card);
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public void removeFromDeck(String suit, String symbol, int value) {
+//		
+//		removeFromDeck(new Card(Suit.valueOf(suit), symbol, value));
+//		
+//	}
+//	
+//	public void add(Card card) {
+//		
+//		if(seenDeck.contains(card)) {
+//			
+//			seenDeck.remove(card);
+//			unseenDeck.add(card);
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public void add(String suit, String symbol, int value) {
+//		
+//		add(new Card(Suit.valueOf(suit), symbol, value));
+//		
+//	}
+//	
+//	public void reset() {
+//		
+//		for(int i = 0; i < seenDeck.size(); i++) {
+//			
+//			unseenDeck.add(seenDeck.get(i));
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		seenDeck.clear();
+//		
+//	}
+//	
+//	public void remove(Card card) {
+//		
+//		if(unseenDeck.contains(card)) {
+//			
+//			unseenDeck.remove(card);
+//			seenDeck.add(card);
+//			
+//		}
+//		
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//	public void remove(String suit, String symbol, int value) {
+//		
+//		remove(new Card(Suit.valueOf(suit), symbol, value));
+//		
+//	}
+//	
+//	public void remove(String symbol) {
+//		
+//		if(unseenDeck.contains(new Card(Suit.SPADES, symbol, getSymbolValue(symbol)))) {
+//			
+//			remove(Suit.SPADES.toString(), symbol, getSymbolValue(symbol));
+//			
+//		} else if(unseenDeck.contains(new Card(Suit.CLUBS, symbol, getSymbolValue(symbol)))) {
+//			
+//			remove(Suit.CLUBS.toString(), symbol, getSymbolValue(symbol));
+//			
+//		} else if(unseenDeck.contains(new Card(Suit.DIAMONDS, symbol, getSymbolValue(symbol)))) {
+//			
+//			remove(Suit.DIAMONDS.toString(), symbol, getSymbolValue(symbol));
+//			
+//		} else if(unseenDeck.contains(new Card(Suit.HEARTS, symbol, getSymbolValue(symbol)))) {
+//			
+//			remove(Suit.HEARTS.toString(), symbol, getSymbolValue(symbol));
+//			
+//		}
+//		
+//	}
+//	
+//	public void undo() {
+//		
+//		Card undoneCard = seenDeck.remove(seenDeck.size() - 1);
+//		unseenDeck.add(undoneCard);
+//		unseenDeck.sort(null);
+//		
+//	}
+//	
+//}
